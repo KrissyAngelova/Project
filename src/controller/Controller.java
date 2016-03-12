4b05b9cd5829e90cc3bb7bc3d70291e15aa9d644
Gratsiela Zhelyazkova <gratsielazhelyazkova@gmail.com>
2016-03-12 20:15:19 +0200

    RegisterServlet

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,20 +6,22 @@ import java.util.List;
 import model.User;
 import model.dao.IUserDao;
 
-public class Controller {
+public class Controller {// pri register vika UserDAO da mu varne spisak s
+							// users, ako ima takav user saobshtava, ako niama
+							// sazdava iuser i vika DAO da dobavi noviat user v
+							// db
 
 	static List<User> users;
 	static IUserDao dao= IUserDao.getDAO(IUserDao.DataSource.DB);
 
-	
 	users=dao.getAllUsers();
-	
-	static boolean regUser(String firstName, String lastName, String email, String password){
-		for(User u:users){
-			if(u.getEmail().equals(email)){
+
+	static boolean regUser(String firstName, String lastName, String email, String password) {
+		for (User u : users) {
+			if (u.getEmail().equals(email)) {
 				return false;
 			}
 		}
-		User newUser= new User(firstName, lastName, email, password);
+		User newUser = new User(firstName, lastName, email, password);
 	}
 }
