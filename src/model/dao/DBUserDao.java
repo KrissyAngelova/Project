bb3f5e7bfbdbf350518814b2b9917fa4bf5c6abb
Krissy Angelova <krisi_aa@windowslive.com>
2016-03-12 15:28:24 +0200

    DBDao

---
+++ src/model/dao/DBUserDao.java
@@ -0,0 +1,50 @@
+package model.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.User;
+
+public class DBUserDao implements IUserDao{
+
+	@Override
+	public void addUser(User x) {
+		try(PreparedStatement st = DBManager.getInstance().
+					getConnection().
+					prepareStatement("INSERT INTO User "
+							+ "(email,firstName,lastName,password, nickName) "
+							+ "VALUES (?, ?, ?, ?, ?)")) {
+			
+			st.setString(1, x.getEmail());
+			st.setString(2, x.getFirstName());
+			st.setString(3, x.getLastName());
+			st.setString(4, x.getPass());
+			st.setString(5, x.getNickname());
+			st.execute();
+			st.close();
+		} catch (SQLException e) {
+			System.out.println("Error executing the statement in addUser:" + e.getMessage());
+		}
+		
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		List<User> registeredUsers = new ArrayList();
+		try( Statement st = new DBManager.getInstance().
+				getConnection().createStatement()){
+			ResultSet rs = st.executeQuery("SELECT email,firstName,lastName,password, nickName"
+					+ "FROM User");
+			while(rs.next()){
+				registeredUsers.add(new User(rs.getString("firstName"), rs.getString("lastName"), 
+						rs.getString("email"), rs.getString("password")));
+			}
+		}
+		return registeredUsers;
+	}
+
+}
